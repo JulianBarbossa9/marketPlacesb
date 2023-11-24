7f18f21d9feaeb730f81c991127fa7177e02d5ff
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-24 12:32:42 -0500

    Refactor NavItems component

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,44 @@
-'use client'
+"use client";
 
-import { useState } from "react"
-
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useRef, useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
-  
-  const [ activeIndex, setActiveIndex ] = useState<null | number>(null)
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  // console.log(activeIndex);
+
+  const isAnyOpen = activeIndex !== null; 
+
+  const navRef = useRef<HTMLDivElement | null>(null)
 
   return (
-    <div className="flex gap-4 h-full">NavItems</div>
-  )
-}
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
 
-export default NavItems
+        const isOpen = i === activeIndex;
+        // console.log(isOpen)
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+            key={category.value}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
