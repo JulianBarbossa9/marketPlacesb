26f5af96c593f8e70f0e36d56c2e0dc9f70d36d2
Julian Barbosa <julianbarbossa@hotmail.com>
2023-12-04 15:53:16 -0500

    Add trpc middleware for creating context and router

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express'
 import { getPayloadClient } from './get-payload'
 import { nextApp, nextHandler } from './next-utils'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { appRouter } from './trpc'
 
 
 const app = express()
 
 const PORT = Number(process.env.PORT) || 3000
+
+//This used for create context for trpc this means we can use req and res in trpc
+const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+})
 
 const start =async () => {
   const payload = await getPayloadClient({
@@ -16,6 +24,12 @@ const start =async () => {
       }
     },
   })
+
+  //This is the middleware for trpc and is used for create context and router
+  app.use('/api/trpc', trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }))
 
   //This is like a middelware
   app.use((req, res) => nextHandler(req, res))
